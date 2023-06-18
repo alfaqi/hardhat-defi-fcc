89c6982f24324526f90689dd8340dea8ed87c07b
alfaqi <alfaqiabdullah@gmail.com>
2023-06-18 14:56:40 +0300

    Modify getDaiPrice and getBorrowUserData functions

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -16,6 +16,23 @@ async function main() {
   await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
   console.log("Deposited!");
 
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+    lendingPool,
+    deployer
+  );
+  const daiPrice = await getDaiPrice();
+  const amountDaiToBorrow =
+    availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber());
+  const amountDaiToBorrowWei = ethers.utils.parseEther(
+    amountDaiToBorrow.toString()
+  );
+  console.log(`You can borrow ${amountDaiToBorrow.toString()} DAI`);
+  await borrowDai(
+    "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    lendingPool,
+    amountDaiToBorrowWei,
+    deployer
+  );
   await getBorrowUserData(lendingPool, deployer);
 }
 
@@ -34,11 +51,11 @@ async function borrowDai(daiAddress, lendingPool, amountDaiToBorrow, account) {
 async function getDaiPrice() {
   const daiEthPriceFeed = await ethers.getContractAt(
     "AggregatorV3Interface",
-    "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5"
+    "0x773616E4d11A78F511299002da57A0a94577F1f4"
   );
 
   const price = (await daiEthPriceFeed.latestRoundData())[1];
-  console.log(`The DAI/ETH price is ${daiEthPriceFeed.toString()}`);
+  console.log(`The DAI/ETH price is ${price.toString()}`);
   return price;
 }
 
@@ -72,6 +89,8 @@ async function getBorrowUserData(lendingPool, account) {
   console.log(`You have ${totalCollateralETH} worth of ETH deposited.`);
   console.log(`You have ${totalDebtETH} worth of ETH borrowed.`);
   console.log(`You can borrow ${availableBorrowsETH} worth of ETH.`);
+
+  return { availableBorrowsETH, totalDebtETH };
 }
 
 main()
